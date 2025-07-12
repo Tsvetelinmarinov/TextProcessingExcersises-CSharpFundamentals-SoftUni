5e179741d32bf3fd704531a92007ecdf7f6bdad7
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-07-12 22:31:51 +0300

    Solusion 3

---
+++ TextProcessingExcersise/03.ExtractFile/Program.cs
@@ -0,0 +1,28 @@
+﻿///<summary>
+///  03.ExtractFile
+/// </summary>
+
+using System;
+
+namespace _03.ExtractFile
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = Console.ReadLine()!;
+
+            (string fileName, string extension) = GetFileNameFormPath(path);
+
+            Console.WriteLine($"File name: {fileName}\nFile extension: {extension}");
+        }
+
+        private static (string fileName, string ext) GetFileNameFormPath(string directory)
+        {
+            string fileNm = directory.Substring(directory.LastIndexOf(@"\") + 1, directory.LastIndexOf('.')  - directory.LastIndexOf(@"\") - 1);
+            string extension = directory.Substring(directory.LastIndexOf('.') + 1);
+
+            return (fileNm, extension);
+        }
+    }
+}
