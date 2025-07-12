a28937e66c277dc5cbc4f7bd2abd8b96afc6f670
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-07-12 21:28:27 +0300

    Solution 2

---
+++ TextProcessingExcersise/02.CharacterMultiplier/Program.cs
@@ -0,0 +1,43 @@
+﻿/// <summary>
+///  02.CharacterMultiplier
+/// </summary>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.CharacterMultiplier
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> names = Console.ReadLine()!
+                .Split()
+                .ToList();
+
+            Console.WriteLine(CombinedCharsSum(names[0], names[1]));
+        }
+
+        private static float CombinedCharsSum(string fString, string sString)
+        {
+            float sum = default;
+            string longer = fString.Length > sString.Length ? fString : sString;
+            string shorter = fString.Length > sString.Length ? sString : fString;
+            
+            for (int i = 0; i < longer.Length; ++i)
+            {
+                if (i <= shorter.Length - 1)
+                {
+                    sum += longer[i] * shorter[i];
+                }
+                else
+                {
+                    sum += longer[i];
+                }
+            }
+
+            return sum;
+        }
+    }
+}
